1d2d1a3d1e8f8d4d90ccaf6f13948308d5d499b1
xiang <yuxiangxiang@sanjieke.com>
2017-10-20 21:31:57 +0800

    构建中的提示

--- web/resources/views/welcome.blade.php
+++ web/resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="flex-center position-ref full-height" id="app">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -79,17 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    @{{ echo }}
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://www.echoecho.cn">www.echoecho.cn</a>
                 </div>
             </div>
         </div>
     </body>
+
+    <script src="/js/app.js"></script>
 </html>
